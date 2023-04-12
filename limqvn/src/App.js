4a778e44adf9fe36e0cec916a1a1d929a67d1cbb
StivLon <124176605+StivLon@users.noreply.github.com>
2023-04-12 11:39:38 -0500

    Proyecto React

---
+++ limqvn/src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+import { Footer } from "./Componentes/Footer";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hola que tal </h1>
+      <h2>Soy la pagina de las poesias, su fiel admirador</h2>
+      <Footer nombre="Carlos" />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+      <Footer />
+    </div>
+  );
+}
